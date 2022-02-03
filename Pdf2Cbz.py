e5f17bd94add0cee37a0a827eb3d47a8b0d17e1e
Antongiacomo Polimeno <antongiacomo.polimeno@unimi.it>
2022-02-03 16:15:48 +0100

    Refactoring

--- Pdf2Cbz.py
+++ Pdf2Cbz.py
@@ -1,62 +1,65 @@
 #!/usr/bin/env python3
 
 import os
-import io
-from PIL import Image
-from pprint import pprint
-from zipfile import ZipFile
 import os.path
-from PyPDF2 import PdfFileMerger, PdfFileReader
-from pdf2image import convert_from_path as pdf_path_to_images
-from pdf2image.exceptions import (
-    PDFInfoNotInstalledError,
-    PDFPageCountError,
-    PDFSyntaxError
-)
-from natsort import os_sorted
-import tempfile
-import zipfile
-import pdb
 import time
+from math import floor
+from zipfile import ZipFile
 
-cores = 6 #os.cpu_count()/2
-out_directory = "out"
-if not os.path.exists(out_directory):
-    os.mkdir("out")
+from natsort import os_sorted
+from pdf2image import convert_from_path as pdf_path_to_images
+
+CORES = floor(os.cpu_count() / 2)
+ROOTDIR = "source"
+
+
 
 def main():
 
+    create_out_directory_if_not_exists("out")
+
     start = time.time()
-    rootdir = 'source' 
-    
-    for subdir, dirs, files in os.walk(rootdir):
+
+
+
+    for subdir, dirs, files in os.walk(ROOTDIR):
         files = os_sorted(files)
 
         for i, file in enumerate(files):
-            
-            images = []
+
             filepath = subdir + os.sep + file
-            print('Start convert {}'.format(filepath))
-            images.extend(pdf_path_to_images(filepath, thread_count= cores , output_folder="out", output_file=f"comic_{i}_", fmt="JPEG" ))
-            print('Finished extracting images from {}'.format(filepath))
+            print("Start convert {}".format(filepath))
+            pdf_path_to_images(
+                filepath,
+                thread_count=CORES,
+                output_folder="out",
+                output_file=f"comic_{i}_",
+                fmt="JPEG",
+            )
+
+            print("Finished extracting images from {}".format(filepath))
 
     mid = time.time()
-    print(f'Mid time: {mid - start}')
-    output_dir = 'out'
-    zipObj = ZipFile('sample.zip', 'w')
+    print(f"Mid time: {mid - start}")
+    output_dir = "out"
+    zipObj = ZipFile("sample.cbz", "w")
 
     for subdir, dirs, files in os.walk(output_dir):
         files = os_sorted(files)
-        for file in files: 
-            filepath = subdir + os.sep + file    
+        for file in files:
+            filepath = subdir + os.sep + file
             zipObj.write(filepath)
 
     zipObj.close()
     end = time.time()
 
-    print(f'Finishing time: {end-start}')
-    
+    print(f"Finishing time: {end-start}")
+
+def create_out_directory_if_not_exists(out_directory):
+    if not os.path.exists(out_directory):
+        os.mkdir(out_directory)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
+
