0afdc5e4a642e305adcdcd7ffca4511d2c74b21a
Leonardo Tartabini <leotartax@hotmail.it>
2022-02-06 00:22:35 +0100

    added handler

--- Pdf2Cbz.py
+++ Pdf2Cbz.py
@@ -9,11 +9,15 @@ from zipfile import ZipFile
 from natsort import os_sorted
 from pdf2image import convert_from_path as pdf_path_to_images
 
+from UpdateHandler import UpdateHandler
+
 class Pdf2Cbz:
     _input_folder_path = None
     _zip_file_name = None
     _image_output_path = None
     _cores_number = floor(os.cpu_count() / 2)
+
+    _update_handler = UpdateHandler()
 
     def __init__(self, input_folder_path, zip_file_name = "pdf2cbz_file.cbz", image_output_path = "out"):
         self._input_folder_path = input_folder_path
@@ -22,6 +26,9 @@ class Pdf2Cbz:
 
     def set_cores_numeber(self, cores_numebr):
         self._cores_number = cores_numebr
+
+    def set_update_handler(self, update_handler: UpdateHandler):
+        self._update_handler = update_handler
 
     def create_out_directory_if_not_exists(self):
         if not os.path.exists(self._image_output_path):
@@ -32,7 +39,7 @@ class Pdf2Cbz:
         print("Start convert {}".format(filepath))
         pdf_path_to_images(
             filepath,
-            thread_count=self._cores,
+            thread_count=self._cores_number,
             output_folder=self._image_output_path,
             output_file=f"comic_{i}_",
             fmt="JPEG",
@@ -41,13 +48,15 @@ class Pdf2Cbz:
 
     def convert_pdf2image_in_folder(self):
         
-        files = select_files_from_path(self._input_folder_path, ".pdf")
+        files, files_count = select_files_from_path(self._input_folder_path, ".pdf")
         
-        for i, file in enumerate(files[0]):
+        for i, file in enumerate(files):
 
             filepath = self._input_folder_path + os.sep + file
 
             self.conver_pdf_page_to_image(filepath, i)
+
+            self._update_handler.update(i,files_count)
 
     def zip_images(self):
 
---
+++ UpdateHandler.py
@@ -0,0 +1,5 @@
+class UpdateHandler:
+    
+    def update(self, i, total_count):
+        print(f"{i+1}/{total_count}")
+
